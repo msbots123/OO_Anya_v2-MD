50823ed396ed18998451255cb7894b6d683b94f3
msbots123 <151300558+msbots123@users.noreply.github.com>
2024-01-09 15:37:46 +0530

    Update config.js

--- config.js
+++ config.js
@@ -79,7 +79,7 @@ module.exports = {
   left: process.env.Left_Msg || 'Ex-member *@$user* is no longer available in this group chat.\n\n_$prefix goodbye off to disable this message._',
   promote: process.env.Promote_Msg || '*@$user* has been promoted as an admin in this group.\n\n_$prefix promotem off to disable this message._',
   demote: process.env.Demote_Msg || '*@$user* has been demoted to a member in this group.\n\n_$prefix demotem off to disable this message._',
-  sessionId: process.env.SESSION_ID || "WU12VTJ_Queen-Anya_RV3k=", 
+  sessionId: process.env.SESSION_ID || "ajE5MnR_Queen-Anya_uVGY=", 
   image_1: readFileSync('./lib/Assets/image_1.jpg'), // Thumbnail for allmenu command
   image_2: readFileSync('./lib/Assets/image_2.jpg'), // null image
   image_3: readFileSync("./lib/Assets/image_3.jpg"), // Thumbnail for Dashboard
